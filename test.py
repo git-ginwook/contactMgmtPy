22fbceb64d6b1df7f76eb5b7a18b589702f40646
ginwooklee_air <leeginw@oregonstate.edu>
2023-02-23 22:28:47 +0900

    minor formatting

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import reminders_middleware as middleware
+import json
+
 
 if __name__ == '__main__':
     data = {"email": "test@gmail.com", "password": "123456", "c_user_id": 0}
     r = middleware.handshake(data["email"], data["password"], data["c_user_id"])
     print(r)
     r2 = middleware.getreminders(data["c_user_id"])
-    print(r2)
+    print(json.dumps(r2, indent=1))
